08332ad4637b4a0abefd368b6ce59640805f0a83
Tiago <frost.tiago@gmail.com>
2024-03-04 15:00:32 -0300

    Avanços no Jogo da Velha

--- MauiAppJogoVelha/MainPage.xaml.cs
+++ MauiAppJogoVelha/MainPage.xaml.cs
@@ -2,12 +2,29 @@
 {
     public partial class MainPage : ContentPage
     {
+        string vez = "O";
+
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button disparador = (Button)sender;
+            
+            disparador.IsEnabled = false;
 
-        
+            if(vez == "O")
+            {
+                disparador.Text = "O";
+                vez = "X";
+            } else
+            {
+                disparador.Text = "X";
+                vez = "O";
+            } // Fecha if
+        } // Fecha Button_Clicked
+
     } // Fecha a classe MainPage
 } // Fecha o Namespace
