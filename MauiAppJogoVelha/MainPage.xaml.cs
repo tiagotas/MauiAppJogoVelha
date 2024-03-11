df954175966b730e1c170be7497eaf5f9a4ade3f
Tiago <frost.tiago@gmail.com>
2024-03-11 15:09:18 -0300

    Limpar o jogo

--- MauiAppJogoVelha/MainPage.xaml.cs
+++ MauiAppJogoVelha/MainPage.xaml.cs
@@ -24,7 +24,23 @@
                 disparador.Text = "X";
                 vez = "O";
             } // Fecha if
+
+            // Verificando se o X ganhou na 1ª Linha
+            if(btn10.Text == "X"
+               && btn11.Text == "X" 
+               && btn12.Text == "X")
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                limpar();
+            }
+
         } // Fecha Button_Clicked
+
+        private void limpar()
+        {
+            btn10.Text = "";
+            btn10.IsEnabled = true;
+        }
 
     } // Fecha a classe MainPage
 } // Fecha o Namespace
